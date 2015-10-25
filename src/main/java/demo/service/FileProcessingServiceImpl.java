8ba3ac22001cf85d31835352dfe716274713abe4
Glenn Bostoen <glenn.bostoen@delawareconsulting.com>
2015-10-25 19:14:13 +0100

    update file format

--- src/main/java/demo/service/FileProcessingServiceImpl.java
+++ src/main/java/demo/service/FileProcessingServiceImpl.java
@@ -7,9 +7,16 @@ import demo.domain.players.PlayerRepository;
 import demo.domain.players.TeamPlayer;
 import demo.domain.players.TeamPlayerRepository;
 import demo.infrastructure.helpers.FileReaderHelper;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,9 +36,17 @@ public class FileProcessingServiceImpl implements FileProcessingService {
     }
 
     public void calculatePointsPlayers(){
-        String fileName = "C:/LEAGUE/MATCHDAYPOINTS.txt";
+        ClassPathResource classPathResource = new ClassPathResource("static/data/MATCHDAYPOINTS.txt");
+
+
         try {
-            List<String> lines = FileReaderHelper.readFile(fileName);
+            InputStream inputStream = classPathResource.getInputStream();
+            BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            List<String> lines = new ArrayList<>();
+            while ((line=r.readLine()) != null) {
+                lines.add(line);
+            }
             lines.stream().filter(x->x.split(",").length == 3).forEach(x -> {
                 String[] values = x.split(",");
                 int day = Integer.parseInt(values[0]);
